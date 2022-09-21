fd6a5a0e69925082036f084a3f806b55772f780a
Robert Long <robertvessnalong@gmail.com>
2022-09-20 22:07:22 -0400

    Complete Rolodex

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import { Component } from "react";
-import logo from "./logo.svg";
+import CardList from "./components/card-list/card-list.component";
 import "./App.css";
+import SearchBox from "./components/search-box/search-box.component";
 
-class App extends Component() {
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      )
+      .catch(() => {});
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'>
-            Learn React
-          </a>
-        </header>
+        <h1 className='app-title'>Monsters Rolodex</h1>
+        <SearchBox
+          className='monster-search-box'
+          onChangeHandler={onSearchChange}
+          placeholder='search'
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
